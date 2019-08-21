0867fb1a509f583ded0087931ea87998e609f2ee
mjimoh4 <mjimoh@montgomerycollege.edu>
2019-08-21 16:00:58 -0400

    Deleted the html file, and fixed the dog.java to fit Exercise 301

--- src/main/java/com/example/demo/Dog.java
+++ src/main/java/com/example/demo/Dog.java
@@ -1,18 +1,24 @@
 package com.example.demo;
 
 import javax.validation.constraints.Min;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
 
+@Entity
 public class Dog
 {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     @NotNull
     @Min(1)
     private long id;
 
 
     @NotNull
-    @Size(min=3, max=20)
+    //@Size(min=3, max=20)
     private String name;
 
     @NotNull
